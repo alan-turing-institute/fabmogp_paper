b6496d097e4c4bf24f3b39d07c8df4e570d41fe0
edaub <edaub@turing.ac.uk>
2020-10-09 18:04:49 +0100

    changed shading in figure 1 pcolor again

--- manuscript/figure1.py
+++ manuscript/figure1.py
@@ -14,7 +14,7 @@ ufault.load()
 fig = plt.figure(figsize=(6.5,3))
 fig.add_subplot(121)
 plt.pcolormesh(vybody.x, vybody.y, vybody.vy, vmin=-2., vmax=2.,
-               shading="auto", cmap="RdBu")
+               shading="nearest", cmap="RdBu")
 plt.plot(ufault.x, ufault.y, "k")
 plt.axis("image")
 plt.xlabel("Position along strike (km)")
